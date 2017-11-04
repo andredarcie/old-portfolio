930343730f3dc6abc729b7e9180861926dbef114
André N. Darcie <andrendarcie@gmail.com>
2017-11-04 00:36:21 -0200

    Change to vuejs

---
+++ data.js
@@ -0,0 +1,295 @@
+var projects = [
+    {
+        title: 'Ruby On Rails Simple Blog',
+        description: 'This project is a blog made based on the official getting started with rails guide.',
+        tags: [
+            'Javascript',
+            'Ruby on Rails'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/ruby-on-rails-simple-blog',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'The Super Tiny Firewall',
+        description: 'Simple firewall made from scratch, written in C language for Linux with a web interface.',
+        tags: [
+            'AngularJS',
+            'C',
+            'JavaEE',
+            'Linux'
+        ],
+        links: [
+            {
+                url: 'http://andredarcie.com/img/portfolio/safe.PNG',
+                label: 'Image'
+            }
+        ]
+    },
+    {
+        title: 'Api for GTFS',
+        description: 'NodeJS API able to provide data in GTFS standard.',
+        tags: [
+            'NodeJS',
+            'ExpressJS',
+            'MongoDB'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/node-gtfs-api',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'Hyper Presentation',
+        description: 'Hyper Presentation is a multimedia content presentation system using smart-TV and interaction with smartphone. Award-winning best project at my university',
+        tags: [
+            'JavaFX',
+            'Java',
+            'Android'
+        ],
+        links: [
+            {
+                url: 'http://andredarcie.com/img/portfolio/hyper-p.png',
+                label: 'Image'
+            }
+        ]
+    },
+    {
+        title: 'GTFS Visualizer',
+        description: 'Library that can draw lines for public transport using leaflet framework based on JSON data in GTFS standard.',
+        tags: [
+            'Bootstrap',
+            'Leaftlet ',
+            'Jquery'
+        ],
+        links: [
+            {
+                url: 'http://andredarcie.com/img/portfolio/hyper-p.png',
+                label: 'Image'
+            }
+        ]
+    },
+    {
+        title: 'C# Simple Guide',
+        description: 'Discovering csharp through code samples.',
+        tags: [
+            'Readme.md',
+            'Leaftlet ',
+            'Jquery'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/csharp-guide',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'Conceptual Art Unity',
+        description: 'A simple example of conceptual art made in unity.',
+        tags: [
+            'C#',
+            'Unity'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/conceptual-art-unity',
+                label: 'Github'
+            },
+            {
+                url: 'http://andredarcie.com/conceptual-art-unity/',
+                label: 'Demo'
+            }
+        ]
+    },
+    {
+        title: 'Instagram like on rails',
+        description: 'Is a instagram like site make with Ruby on Rails.',
+        tags: [
+            'Ruby on Rails',
+            'Bootstrap Sass',
+            'Devise',
+            'Paperclip',
+            'Image Magick'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/instagram-like-on-rails',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'Simple Asp Net Core 2: Todo App',
+        description: 'Simple asp net core todo app based on LittleAspNetCoreBook.',
+        tags: [
+            '.Net Core',
+            'C#'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/simple-asp-net-core-2-todo-app',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: '2D Roguelike Game',
+        description: '2D game created in Unity 5',
+        tags: [
+            'C#',
+            'Unity'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/2d-roguelike-game-unity',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'History of Video Games',
+        description: 'Timeline containing a list of games that have consistently been ranked among the best by video game jornalists, divided by generations.',
+        tags: [
+            'Javascript',
+            'VueJS',
+            'CSS'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/history-of-video-games',
+                label: 'Github'
+            },
+            {
+                url: 'http://andredarcie.com/history-of-video-games/',
+                label: 'Demo'
+            }
+        ]
+    },
+    {
+        title: 'Catho Crawler',
+        description: 'NodeJS Crawler for Catho based on request and cheerio.',
+        tags: [
+            'Javascript',
+            'Heroku',
+            'NodeJS'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/catho-crawler',
+                label: 'Github'
+            },
+            {
+                url: 'https://catho-crawler.herokuapp.com/',
+                label: 'Demo'
+            }
+        ]
+    },
+    {
+        title: 'Mysterious Game',
+        description: 'Top Secret.',
+        tags: [
+            'Javascript',
+            'Heroku',
+            'NodeJS'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/catho-crawler',
+                label: 'Github'
+            },
+            {
+                url: 'https://catho-crawler.herokuapp.com/',
+                label: 'Demo'
+            }
+        ]
+    },
+    {
+        title: 'Get Out Game',
+        description: 'Simple player movement and animation with Phaser.',
+        tags: [
+            'Javascript',
+            'Phaser'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/get-out-game',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'My First Web Site',
+        description: 'This was the first web site I created, it was here that I learned HTML and CSS.',
+        tags: [
+            'Javascript',
+            'HTML'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/my-first-web-site',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'Shopping Cart App',
+        description: 'Shopping Cart app make with Ruby on Rails.',
+        tags: [
+            'Ruby on Rails'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/ruby-on-rails-shopping-cart',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'Awesome GTFS',
+        description: 'A collection of awesome GTFS libraries, resources and other things.',
+        tags: [
+            'Readme.md'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/awesome-gtfs',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'Ionic Weather App',
+        description: 'App that discovers temperature based on user position.',
+        tags: [
+            'Ionic',
+            'Javascript',
+            'NodeJS'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/awesome-gtfs',
+                label: 'Github'
+            }
+        ]
+    },
+    {
+        title: 'Mobile Public Transport App',
+        description: 'Aplicativo móvel, construído com Phonegap para visualização e planejamento de linhas de ônibus para a cidade de Poços de Caldas, Minas Gerais.',
+        tags: [
+            'Javascript',
+            'Phonegap'
+        ],
+        links: [
+            {
+                url: 'https://github.com/andredarcie/mobile-public-transport',
+                label: 'Github'
+            }
+        ]
+    }
+];
