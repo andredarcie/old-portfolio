5c3f93cbbfa5a42f2e323e8e927ecce07499d964
andredarcie <andrendarcie@gmail.com>
2018-10-06 11:39:33 -0300

    Add tracker

---
+++ tracker-data.js
@@ -0,0 +1,105 @@
+var trackerData = [{
+    type: "games",
+    displayName: "Video games",
+    show: false,
+    list: [{
+        year: "2011",
+        list: [{
+            score: 1,
+            name: "Braid"
+        }, {
+            score: 2,
+            name: "Call of Duty 4: Modern Warfare"
+        }],
+        todoList: [
+            {
+                name: "Braid"
+            }
+        ]
+    }, {
+        year: "2012",
+        list: [{
+            score: 1,
+            name: "Braid"
+        }, {
+            score: 2,
+            name: "Call of Duty 4: Modern Warfare"
+        }]
+    }]
+},
+{
+    type: "movies",
+    displayName: "Movies",
+    show: false,
+    list: [{
+        year: "2011",
+        list: [{
+            score: 2,
+            name: "The Godfather"
+        }, {
+            score: 3,
+            name: "City of god"
+        }]
+    }]
+},
+{
+    type: "tv",
+    displayName: "Tv series",
+    show: false,
+    list: [{
+        year: "2011",
+        list: [{
+            score: 2,
+            name: "Dexter"
+        }, {
+            score: 3,
+            name: "The Sopranos"
+        }]
+    }]
+},
+{
+    type: "book",
+    displayName: "Books",
+    show: false,
+    list: [{
+        year: "2011",
+        list: [{
+            score: 2,
+            name: "Divine"
+        }, {
+            score: 3,
+            name: "Comedy"
+        }]
+    }]
+},
+{
+    type: "comics",
+    displayName: "Comics",
+    show: false,
+    list: [{
+        year: "2011",
+        list: [{
+            score: 2,
+            name: "Watchmen"
+        }, {
+            score: 3,
+            name: "Batman"
+        }]
+    }]
+},
+{
+    type: "documentaries",
+    displayName: "Documentaries",
+    show: false,
+    list: [{
+        year: "2011",
+        list: [{
+            score: 2,
+            name: "Watchmen"
+        }, {
+            score: 3,
+            name: "Batman"
+        }]
+    }]
+}
+]
